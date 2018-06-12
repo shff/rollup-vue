d18e5be08b2edd630a77453aa1d4a2cf1e30fd80
Silvio Henrique Ferreira <shferreira@me.com>
2018-06-11 23:22:39 -0300

    Fix linter errors

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
 import Vue from "vue";
-import Tachyons from "tachyons/css/tachyons.css";
+import "tachyons/css/tachyons.css";
 
 new Vue({ render: h => h("div", "Hello World") }).$mount("main");
--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,15 @@ import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 import serve from "rollup-plugin-serve";
-import babel from 'rollup-plugin-babel';
+import babel from "rollup-plugin-babel";
 import vue from "rollup-plugin-vue";
-import { uglify } from 'rollup-plugin-uglify';
+import { uglify } from "rollup-plugin-uglify";
 
 import simplevars from "postcss-simple-vars";
 import nested from "postcss-nested";
-import cssnext from "postcss-cssnext";
 import cssnano from "cssnano";
 
-const watch = process.env.ROLLUP_WATCH == "true";
+const watch = process.env.ROLLUP_WATCH === "true";
 
 export default {
   input: "app/main.js",
@@ -21,37 +20,39 @@ export default {
     name: "RollupVueApp",
     file: "dist/bundle.js",
     strict: true,
-    format: "iife"
+    format: "iife",
   },
   watch: {
-    include: "app/**"
+    include: "app/**",
   },
   plugins: [
     watch && livereload(),
     purifycss({
-      content: ["main/**/*.js", "main/**/*.html"],
+      content: [
+        "main/**/*.js",
+        "main/**/*.html",
+      ],
       options: {
-        minify: true
-      }
+        minify: true,
+      },
     }),
     postcss({
-      "plugins": [
+      plugins: [
         simplevars(),
         nested(),
-        cssnano()
-      ]
+        cssnano(),
+      ],
     }),
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
-      ENV: JSON.stringify(process.env.NODE_ENV || "development")
     }),
     resolve(),
     uglify(),
     babel({ exclude: "node_modules/**" }),
     watch && serve({
       open: true,
-      contentBase: "dist"
+      contentBase: "dist",
     }),
-    vue()
-  ]
+    vue(),
+  ],
 };
